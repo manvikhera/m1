481034ef877d7de0065ce0d11417ca705f05626b
manvikhera <kheramanvis@gmail.com>
2024-08-13 16:40:48 +0530

    Add files via upload

---
+++ flags.js
@@ -0,0 +1,10 @@
+dis(){
+    document.getElementById("a").innerHTML = "The flag"
+
+    //document.getElementById("a").innerHTML = "https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/640px-Flag_of_Brazil.svg.png"
+}
+
+out(){
+    document.getElementById("a").innerHTML = "The flag"
+
+}
